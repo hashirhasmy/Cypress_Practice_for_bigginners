dbad8e8ed22faa6759e54eae864f0fc0890b490b
Mohamed Hashir <hashirhasmy111@gmail.com>
2023-05-23 19:17:08 +0530

    for test

--- cypress/e2e/CSSLocators.cy.js
+++ cypress/e2e/CSSLocators.cy.js
@@ -5,8 +5,6 @@ describe('CSSlocators_Practice', function(){
        cy.visit('http://automationpractice.pl/index.php')
        cy.get("[id='search_query_top']").type("T-Shirts")
        cy.get("[name='submit_search']").click()
-        
-
        cy.get(".lighter").contains("T-Shirts")  //Assertion
         
 
