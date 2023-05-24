1a06f114cfd07b62d53b5c0598556412957a299e
Mohamed Hashir <hashirhasmy111@gmail.com>
2023-05-24 19:07:47 +0530

    2023.05.24 2

--- cypress/e2e/Assertion.cy.js
+++ cypress/e2e/Assertion.cy.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 
 describe("Assertion demo", function(){
 
@@ -17,6 +19,8 @@ describe("Assertion demo", function(){
             cy.url().should('eq','https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
             .and('include','orangehrmlive.com/web/')
             .and('contain','orangehrmlive')
+
+            cy.get()
 
             
 
