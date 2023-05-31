8c7251396d225904c1c5457c59347d9e8644b1ce
Mohamed Hashir <hashirhasmy111@gmail.com>
2023-05-31 15:28:43 +0530

    01

--- cypress/e2e/Iframe.cy.js
+++ cypress/e2e/Iframe.cy.js
@@ -9,16 +9,15 @@ describe("Working with IFreame", ()=> {
         cy.get("button[title='Bold']").click()
     })
 
-    it.skip("hanling iframe approach 02 by using custom command", () => {
+    it.only("hanling iframe approach 02 by using custom command", () => {
         cy.visit("https://the-internet.herokuapp.com/iframe")
         cy.getIframe("#mce_0_ifr").clear().type("Welcome {ctrl+a}")
         cy.get("button[title='Bold']").click()
     })
 
 
-    it.only("hanling iframe approach 03 by using IframePlugin", () => {
+    it.skip("hanling iframe approach 03 by using IframePlugin", () => {
         cy.visit("https://the-internet.herokuapp.com/iframe")
-        
     })
 
 
