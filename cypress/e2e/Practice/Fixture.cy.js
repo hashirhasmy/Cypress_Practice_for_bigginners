5fc08d121e9c09450f973e222a02bf7e4c629f24
Mohamed Hashir <hashirhasmy111@gmail.com>
2023-06-05 20:05:47 +0530

    2023.06.05 2nd change

--- cypress/e2e/Practice/Fixture.cy.js
+++ cypress/e2e/Practice/Fixture.cy.js
@@ -1,15 +1,34 @@
+/// <reference types="cypress-xpath" />
+
 describe("fixture demo test", () => {
 
-    it("login to opencart appliocation", () => {
+    it("login to opencart appliocation using fixture aproach 01", () => {
 
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
-        cy.fixture('orangeHRM').then((data)=> {
-            cy.get("input[placeholder='Username']").type(data.username)
-            cy.get("input[placeholder='Password']").type(data.password)
-            cy.get("button[type='submit']").click()
+        cy.fixture('orangeHRM').then((data) => {
+            cy.get("input[placeholder='Username']").type(data.username);
+            cy.get("input[placeholder='Password']").type(data.password);
+            cy.get("button[type='submit']").click();
+            cy.xpath("//h6[text()='Dashboard']").should('have.text', data.expected);
         })
-
-        
     })
+
+
+    let userData;
+    before(() => {
+        cy.fixture('orangeHRM').then((data01) => {
+            userData = data01;
+        })
+    })
+
+    it("login to opencart appliocation using fixture aproach 02", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.get("input[placeholder='Username']").type(userData.username);
+        cy.get("input[placeholder='Password']").type(userData.password);
+        cy.get("button[type='submit']").click();
+        cy.xpath("//h6[text()='Dashboard']").should('have.text', userData.expected);
+    })
+
+
 })
